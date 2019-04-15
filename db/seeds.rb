f05e3be04a9be26537ace7ef15e7c03cae06d1c9
tpstar <hanlee12@gmail.com>
2019-04-14 22:25:07 -0500

    integrate index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ puts "5 skills created"
     title: "Portfolio title #{portfolio_item}",
     subtitle: "My great service",
     body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    main_image: "https://via.placeholder.com/350x200",
+    main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
   )
 end
